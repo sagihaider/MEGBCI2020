e6cefb1cf22dc386bf00c0fd529845cadf114b38
Haider Raza <sagihaider@gmail.com>
2021-01-02 21:09:19 +0000

    Create BoxPlot_CV.m

---
+++ BoxPlot_CV.m
@@ -0,0 +1,15 @@
+% Code by @ Dr Haider Raza
+% Institute for Analytics and Data Science, School of Computing and
+% Electronics Engineering, University of Essex, Colchester, England
+% contact: h.raza@essex.ac.uk
+% Date: 13/05/2019
+
+% Classes: 1-Both Hand Imagery, 2-Both Feet Imagery
+% 3-Word generation, 4-Subtraction
+close all; clc;
+clear;
+warning off
+
+%% PATH to data
+currFolder = pwd;
+addpath(genpath(currFolder));
