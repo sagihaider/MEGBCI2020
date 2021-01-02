ddc060333d99dd8a398fe6eb24c77d250507fc74
Haider Raza <sagihaider@gmail.com>
2021-01-02 21:29:28 +0000

    haider

--- BoxPlot_CV.m
+++ BoxPlot_CV.m
@@ -13,3 +13,35 @@ warning off
 %% PATH to data
 currFolder = pwd;
 addpath(genpath(currFolder));
+
+% acc_cv_mb = load('acc_cv_mubeta.mat');
+% acc_cv_fbcsp = load('acc_cv_fbcsp.mat');
+% boxplot([acc_cv_mb.cv_acc,acc_cv_fbcsp.cv_acc])
+
+% 
+acc_test_mb = load('acc_test_mubeta.mat');
+acc_test_fbcsp = load('acc_test_fbcsp.mat');
+% % boxplot([acc_test_mb.acc_sub, acc_test_fbcsp.acc_sub])
+% 
+% 
+% boxplot(acc_test_mb.acc_sub,'Positions',xc-offset,'Widths',w,'Colors','r');
+% boxplot(acc_test_fbcsp.acc_sub,'Positions',xc+offset,'Widths',w,'Colors','k');
+
+% Data
+
+x = 1:5;
+y = randn(5, 3, 100);
+
+% Plot boxplots
+
+h = boxplot2(y,x);
+
+% Alter linestyle and color
+
+cmap = get(0, 'defaultaxescolororder');
+for ii = 1:3
+    structfun(@(x) set(x(ii,:), 'color', cmap(ii,:), ...
+        'markeredgecolor', cmap(ii,:)), h);
+end
+set([h.lwhis h.uwhis], 'linestyle', '-');
+set(h.out, 'marker', '.');
